ce960a49f0317e962a65782e31be2c15b4e09745
AntoniaF484 <antoniaforsythe16@gmail.com>
2026-01-30 16:40:39 +0000

    Office Scene - Player Can move in the space and UI triggered by spacebar

---
+++ Assets/Scripts/OfficeGameplay/DialogueManager.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using Ink.Runtime;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.InputSystem;
+using System.Collections.Generic;
+public class DialogueManager : MonoBehaviour
+{
+    public TextAsset inkFile;
+    public TextMeshProUGUI textBox;
+    private Story story;
+
+    public Button[] choiceButtons;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        story = new Story(inkFile.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (UnityEngine.InputSystem.Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            ContinueStory(); // text progresses at players own pace
+        }
+    }
+
+    public void StartDialogue() 
+    { 
+        story = new Story(inkFile.text); ContinueStory();
+    }
+    private void ContinueStory()
+    {
+        if (story.canContinue)
+        {
+            textBox.gameObject.SetActive(true);
+            textBox.text = story.Continue();
+            ShowChoices();
+        }
+        else
+        {
+            FinishDialogue();
+        }
+    }
+
+    private void ShowChoices()
+    {
+        List<Choice> choices = story.currentChoices;
+        int index = 0;
+        foreach (Choice c in choices)
+        {
+            choiceButtons[index].GetComponentInChildren<TextMeshProUGUI>().text = c.text;
+            choiceButtons[index].gameObject.SetActive(true);
+            index++;
+        }
+
+        for (int i = index; i < choiceButtons.Length; i++)
+        {
+            choiceButtons[i].gameObject.SetActive(false);
+        }
+    }
+
+    public void SetDecision(int choiceIndex)
+    {
+        story.ChooseChoiceIndex(choiceIndex);
+        ContinueStory();
+    }
+    private void FinishDialogue()
+    {
+        textBox.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/OfficeGameplay/PlayerController.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+// Controls player movement and rotation.
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;          // Set player movement speed.
+    public float rotationSpeed = 120.0f; // Set player rotation speed.
+
+    private Rigidbody rb; 
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    
+    private void FixedUpdate()
+    {
+        float moveVertical = 0f;
+        float turn = 0f;
+
+        // Vertical movement (W / S)
+        if (Keyboard.current.wKey.isPressed)
+            moveVertical += 1f;
+
+        if (Keyboard.current.sKey.isPressed)
+            moveVertical -= 1f;
+
+        // Horizontal rotation (A / D)
+        if (Keyboard.current.dKey.isPressed)
+            turn += 1f;
+
+        if (Keyboard.current.aKey.isPressed)
+            turn -= 1f;
+
+        // Move player forward/backward
+        Vector3 movement = transform.forward * moveVertical * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + movement);
+
+        // Rotate player left/right
+        float rotationAmount = turn * rotationSpeed * Time.fixedDeltaTime;
+        Quaternion turnRotation = Quaternion.Euler(0f, rotationAmount, 0f);
+        rb.MoveRotation(rb.rotation * turnRotation);
+    }
+
+}
