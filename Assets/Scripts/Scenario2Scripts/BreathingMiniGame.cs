d88d48f018e18ca1202e76c93fc2faeb0bd87434
ataskerlynch <146075853+ataskerlynch@users.noreply.github.com>
2026-02-04 18:49:49 +0000

    Breathing Game Update

--- Assets/Scripts/Scenario2Scripts/BreathingMiniGame.cs
+++ Assets/Scripts/Scenario2Scripts/BreathingMiniGame.cs
@@ -12,41 +12,61 @@ public class BreathingMiniGame : MonoBehaviour
     public float calmGainRate = 0.4f;    // How fast calm increases
     public float calmLossRate = 0.25f;   // How fast calm decreases
 
+    [Header("Stress Interaction")]
+    [SerializeField] private StressLevelController stressLevelController;
+    [SerializeField] private float secondsPerStressDown = 1.5f;
+    [SerializeField] private float secondsPerStressUp = 1.5f;
+
     private float calmValue = 0f; // 0 = panic, 1 = calm
+    private float stressReduceTimer = 0f;
+    private float stressTimer = 0f;
 
     void Update()
     {
-        if (GamePause.Mode != PauseMode.Hard)
+        float featherPos = feather.GetNormalizedBreath();
+        float playerPos = cursor.GetNormalizedHeight();
+
+        float distance = Mathf.Abs(featherPos - playerPos);
+
+        //float match = Mathf.InverseLerp(0.4f, 0f, distance);
+        Debug.Log(distance);
+        bool isMatching = distance <= successRange;
+
+        if (isMatching)
         {
-            float featherPos = feather.GetNormalizedBreath();
-            float playerPos = cursor.GetNormalizedHeight();
+            Debug.Log("Matching");
+            calmValue += calmGainRate * Time.unscaledDeltaTime;
 
-            float distance = Mathf.Abs(featherPos - playerPos);
-
-            float match = Mathf.InverseLerp(0.4f, 0f, distance);
-
-            bool isMatching = distance <= successRange;
-
-            if (isMatching)
+            stressTimer += Time.unscaledDeltaTime;
+            if (stressTimer >= secondsPerStressDown)
             {
-                //Debug.Log("Matching");
-                calmValue += calmGainRate * Time.unscaledDeltaTime;
+                //stressTimer = 0;
+                stressTimer -= secondsPerStressDown;
+                stressLevelController.ReduceStress(1);
             }
-            else
+        }
+        else
+        {
+            Debug.Log("Not Matching");
+            calmValue -= calmLossRate * Time.unscaledDeltaTime;
+
+            stressTimer += Time.unscaledDeltaTime;
+            if (stressTimer >= secondsPerStressUp)
             {
-                //Debug.Log("Not Matching");
-                calmValue -= calmLossRate * Time.unscaledDeltaTime;
+                stressTimer -= secondsPerStressUp;
+                //stressTimer = 0;
+                stressLevelController.AddStress(1);
             }
+        }
 
-            calmValue = Mathf.Clamp01(calmValue);
-            calmSlider.value = calmValue;
+        calmValue = Mathf.Clamp01(calmValue);
+        calmSlider.value = calmValue;
 
 
 
-            if (calmValue >= 1f)
-            {
-                OnCalmedDown();
-            }
+        if (calmValue >= 1f)
+        {
+            OnCalmedDown();
         }
     }
 
